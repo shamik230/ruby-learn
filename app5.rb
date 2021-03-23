5d866e5d5dc1414ff2c135811ddbd1b90c858ab3
shamik230 <shamik230@gmail.com>
2021-03-23 17:41:16 +0300

    app5 add

---
+++ app5.rb
@@ -0,0 +1,37 @@
+arr = [:rock, :scissors, :paper]
+
+puts '(R)ock, (S)cissors, (P)aper? '
+s = gets.strip.capitalize
+
+user_choice = nil
+user_choice = :rock if s == 'R'
+user_choice = :scissors if s == 'S'
+user_choice = :paper if s == 'P'
+exit if user_choice.nil?
+
+computer_choice = arr[rand(0..2)]
+
+puts "You picked a #{user_choice}, computer picked a #{computer_choice}"
+
+combs =
+[
+    [:rock, :rock, 'Draw'],
+    [:scissors, :scissors, 'Draw'],
+    [:paper, :paper, 'Draw'],
+
+    [:rock, :scissors, 'Player wins!'],
+    [:rock, :paper, 'Computer wins!'],
+
+    [:scissors, :paper, 'Player wins!'],
+    [:scissors, :rock, 'Computer wins!'],
+
+    [:paper, :rock, 'Player wins!'],
+    [:paper, :scissors, 'Computer wins!']
+
+]
+
+combs.each do |user, computer, result|
+    if user_choice == user && computer_choice == computer
+        puts "Result: #{result}"
+    end
+end
