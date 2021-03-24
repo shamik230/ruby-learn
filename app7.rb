057d9d70224885c7b4ca7d13e3e201c788b07e6c
shamik230 <shamik230@gmail.com>
2021-03-24 19:38:34 +0300

    kak je ya zaebalsya

--- app7.rb
+++ app7.rb
@@ -4,30 +4,64 @@ table = [
     ['*', '*', '*'],
     ['*', '*', '*']
 ]
+
 def show_table(table)
     table.each { |y| y.each {|x| print "#{x} "}; puts }
 end
+
 def free?(x, y, table)
-    if table[y][x] != '*'
+    if table_full?(table)
+        puts "Игра закончена! (Ничья)"
+        exit
+    elsif table[y][x] != '*'
         return false
     else
         return true
     end
 end
+
+def table_full?(table)
+    table.each do |y|
+        y.each do |x|
+            if x == '*'
+                return false
+            end
+        end
+    end
+    return true
+end
+
 def player_turn(table)
-    puts "Ваш ход по X: "
-    player_x = gets.to_i
-    puts "Ваш ход по Y: "
-    player_y = gets.to_i
-    if free?(player_x, player_y, table)
-        table[player_y][player_x] = 'X' 
-    else
-        puts "Ячейка уже занята!"
+    loop do
+        puts '=' * 15
+        puts "Ваш ход по X: "
+        player_x = gets.to_i
+        puts "Ваш ход по Y: "
+        player_y = gets.to_i
+        puts '=' * 15
+        if !([1,2,3].include?(player_x) && [1,2,3].include?(player_y))
+            puts 'Неверный ввод!'
+            next
+        elsif free?(player_x - 1, player_y - 1, table)
+            table[player_y - 1][player_x - 1] = 'X'
+            break 
+        else
+            puts "Ячейка уже занята!"
+        end
     end
 end
+
 def computer_turn(table)
-    
+    loop do
+        computer_x = rand(0..2)
+        computer_y = rand(0..2)
+        if free?(computer_x, computer_y, table)
+            table[computer_y][computer_x] = 'O'
+            break
+        end
+    end
 end
+
 loop do
     show_table(table)
     player_turn(table)
