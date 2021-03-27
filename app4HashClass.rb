60f30821561795bfcd3a10ab93da3e0f25010a34
shamik230 <shamik230@gmail.com>
2021-03-27 22:30:40 +0300

    app4 with class

---
+++ app4HashClass.rb
@@ -0,0 +1,34 @@
+# Реализация app4 с классом
+class Book
+    def initialize
+        @hash = {}
+    end
+
+    def input
+        puts '=' * 30
+        print 'Введите имя: '
+        name = gets.strip.capitalize
+        if name.empty?
+            show()
+            exit
+        elsif @hash[name]
+            puts 'Такое имя уже есть!'
+            puts '=' * 30
+            return
+        end
+        print 'Введите номер: '
+        tel = gets.strip
+        @hash[name] = tel
+        puts '=' * 30
+    end
+
+    def show
+        @hash.each { |k, v| puts "Name: \t#{k} \nTel: \t#{v}"; puts '-' * 30 } 
+    end
+end
+
+book1 = Book.new
+loop do
+    book1.input
+    book1.show
+end
