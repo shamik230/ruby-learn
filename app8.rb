88967f84595bcca299b31d40f057ee7925bf38a1
shamik230 <shamik230@gmail.com>
2021-03-28 01:01:37 +0300

    bubble sort))

---
+++ app8.rb
@@ -0,0 +1,32 @@
+# Сортировка массива пузырьком owo
+def arrayRnd(array) # Заполняем массив в рандомном порядке от 1 до (sizeof array) (каждое число встречается один раз)
+    already_here = []
+    i = 0
+    loop do
+        rnd = rand(1..array.size)
+        if !already_here.any?(rnd)
+            array[i] = rnd
+            already_here << rnd
+            i += 1
+        elsif i == array.size
+            break
+        end
+    end
+    return array
+end
+
+def cringe_sort(array) # Сортировка
+    array.each_index do
+        0.upto(array.size - 2) do |i|
+            j = i + 1
+            array[i], array[j] = array[j], array[i] if array[i] > array[j]
+        end
+    end
+    return array
+end
+
+arr = Array.new(10)
+arrayRnd(arr)
+puts "До: \t#{arr.inspect}"
+cringe_sort(arr)
+puts "После: \t#{arr.inspect}"
