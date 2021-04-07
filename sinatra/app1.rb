56dc91f4283b6ee62c97bec277374564430722d5
shamik230 <shamik230@gmail.com>
2021-04-07 15:47:07 +0300

    it works

--- sinatra/app1.rb
+++ sinatra/app1.rb
@@ -1,7 +1,5 @@
 require 'sinatra'
 
-$hash = {}
-
 get '/' do
     erb :index
 end
@@ -11,6 +9,7 @@ get '/admin' do
 end
 
 get '/welcome' do
+    @data = File.open("./users.txt", "r")
     erb :welcome
 end
 
@@ -18,12 +17,12 @@ post '/' do
     @username = params[:username]
     @tel = params[:tel]
     @time = params[:time]
-    file = File.open("./users.txt", "a")
-    if @username.length > 3
-        $hash[@username] = [@tel, @time]
-        file.write("Name: #{@username} Tel: #{@tel} Time: #{@time}\n")
+    @file = File.open("./users.txt", "a")
+    if @username.length > 0
+        @file.write("Name:#{@username} Tel:#{@tel} Time:#{@time}\n")
         @index_message = "Ваши данные сохранены"
     end
+    @file.close
     erb :index
 end
 
@@ -31,7 +30,7 @@ post '/admin' do
     @login = params[:admin_login]
     @password = params[:admin_password]
     if @login == "admin" && @password == "secret"
-        erb :welcome
+        redirect "/welcome"
     else
         @admin_message = "Access denied"
         erb :admin
