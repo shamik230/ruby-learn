57223b52d435dec4f7ea646f5979b257310c4d1a
shamik230 <shamik230@gmail.com>
2021-04-23 00:30:07 +0300

    minus govno code

--- prikoli/app_bubbleSort.rb
+++ prikoli/app_bubbleSort.rb
@@ -1,32 +1,23 @@
 # Сортировка массива пузырьком owo
-def arrayRnd(array) # Заполняем массив в рандомном порядке от 1 до array.size (каждое число встречается один раз)
-    already_here = []
-    i = 0
-    loop do
-        rnd = rand(1..array.size)
-        if !already_here.any?(rnd)
-            array[i] = rnd
-            already_here << rnd
-            i += 1
-        elsif i == array.size
-            break
-        end
-    end
-    return array
+def arrayRnd(array, size) # Заполняем массив в рандомном порядке от 1 до array.size (каждое число встречается один раз)
+    array = [*1..size]
+    array.shuffle!
 end
 
 def cringe_sort(array) # Сортировка
-    array.each_index do
         0.upto(array.size - 2) do |i|
             j = i + 1
             array[i], array[j] = array[j], array[i] if array[i] > array[j]
         end
-    end
     return array
 end
 
-arr = Array.new(10)
-arrayRnd(arr)
+arr = []
+arr = arrayRnd(arr, 10)
 puts "До: \t#{arr.inspect}"
-cringe_sort(arr)
-puts "После: \t#{arr.inspect}"
+loop do
+    gets
+    cringe_sort(arr)
+    puts "После: \t#{arr.inspect}"
+    exit if arr == arr.sort
+end
